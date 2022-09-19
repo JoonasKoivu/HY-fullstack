9fcd4c1cf5ec869efa4c69d7eb2293ab2fd3bddc
JoonasKoivu <34013378+JoonasKoivu@users.noreply.github.com>
2022-09-19 17:32:14 +0300

    Delete osa2/maat directory

--- osa2/maat/filter.js
+++
@@ -1,8 +0,0 @@
-import React from 'react'
-
-const Filter = ({filter, onFilterChange}) => {
-    return(
-        <p>Find countries:  <input value={filter} onChange={onFilterChange}/></p>
-    )
-}
-export default Filter
