d93d5ec850c94ad01d246b275687a64419fbe1e4
Joonas <34013378+JoonasKoivu@users.noreply.github.com>
2022-12-15 21:28:19 +0200

    Create anecdotes.js

---
+++ osa6/redux-anecdotes/src/services/anecdotes.js
@@ -0,0 +1,24 @@
+import axios from 'axios'
+
+const baseUrl = 'http://localhost:3001/anecdotes'
+
+const getId = () => (100000 * Math.random()).toFixed(0)
+
+const getAll = async () => {
+    const response = await axios.get(baseUrl)
+    return response.data
+}
+
+const createNew = async (content) => {
+    console.log("content in createNew", content)
+    const object = {content, votes: 0, id: getId()}
+    const response = await axios.post(baseUrl, object)
+    return response.data
+}
+
+const likeAnecdote = async (id, newObject) => {
+    const response = await axios.put(`${baseUrl}/${id}`, newObject)
+    return response.data
+}
+
+export default { getAll, createNew, likeAnecdote}
